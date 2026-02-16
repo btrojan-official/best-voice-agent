630c3b40969a0214e0ca8cb765363c08d747be55
btrojan-official <bartosztrojanofficial@gmail.com>
2026-02-16 21:09:17 +0100

    fix: removed unused function

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,12 +1,7 @@
-from .query_llm import query_openrouter, text_to_speech, transcribe_audio, test_pipeline
 from .transcription import transcribe_audio_stream, transcribe_audio_file
 from .tts import text_to_speech_stream, text_to_speech_file
 
 __all__ = [
-    "query_openrouter",
-    "text_to_speech",
-    "transcribe_audio",
-    "test_pipeline",
     "transcribe_audio_stream",
     "transcribe_audio_file",
     "text_to_speech_stream",
